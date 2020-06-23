c70ad9b6d619e1f348a3216343c8a5518dbcd304
cym2050 <1390062269@qq.com>
2020-06-23 16:54:16 +0800

    get robots' information grom api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import CardList from './CardList';
-import { robots } from './robots';
 import SearchBox from './SearchBox';
 import './App.css'
 
@@ -9,7 +8,7 @@ class App extends React.Component {
 	constructor(){
 		super();
 		this.state = {
-			robots: robots,
+			robots: [],
 			searchfiled: ''
 		}
 	}; 
@@ -18,18 +17,30 @@ class App extends React.Component {
 		this.setState({ searchfiled: event.target.value });
 	}
 
+	componentDidMount() {
+		fetch('https://jsonplaceholder.typicode.com/users')
+			.then(response => response.json())
+			.then(users => this.setState({ robots: users}))
+	}
+
 	render() {
 		const filteredRobots = this.state.robots.filter(robot => {
 			return robot.name.toLowerCase().includes(this.state.searchfiled.toLowerCase())
 		})
-		return(
-			<div className='tc'>
-				<h1 className='f1'>RoboFriends</h1>
-				<SearchBox  searchChange={this.onSearchChange} />
-				<CardList robots={filteredRobots}/>
-				
-			</div>
-		)
+
+		if(this.state.robots.length === 0) {
+			return <h1 className='tc'>Loading...</h1>
+		} else {
+			return(
+				<div className='tc'>
+					<h1 className='f1'>RoboFriends</h1>
+					<SearchBox  searchChange={this.onSearchChange} />
+					<CardList robots={filteredRobots}/>
+					
+				</div>
+			)
+		}
+		
 	}
 	
 }
