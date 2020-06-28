f700f5542921c9c40e1b3e0c3680fb04ee485bd3
cym2050 <1390062269@qq.com>
2020-06-28 23:39:39 +0800

    Add Errorboundry

--- src/CardList.js
+++ src/CardList.js
@@ -5,7 +5,6 @@ function CardList({ robots }) {
 	const cardArray = robots.map((user, i) => {
 		return <Card key={robots[i].name} id={robots[i].id} name={robots[i].name} email={robots[i].email} />
 	})
-	
 	return (
 		<div>
 			{ 
---
+++ src/components/ErrorBoundry.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+
+class ErrorBoundry extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			hasError: false
+		}
+	}
+
+	componentDidCatch(error, info) {
+		this.setState({ hasError: true });
+	}
+
+	render() {
+		if(this.state.hasError) {
+			return <h1>Oooops. That is not good</h1>;
+		} else {
+			return this.props.children;
+		}
+		console.log('hello hasError');
+	}
+}
+
+export default ErrorBoundry;
